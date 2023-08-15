1113a89e4bc8fd6c04fb4bf6c736798e0d4748f4
randy steven <randysteven12@gmail.com>
2023-08-15 18:41:58 +0700

    main class

--- main-application/src/main/java/com/main/application/demo/MainApplication.java
+++ main-application/src/main/java/com/main/application/demo/MainApplication.java
@@ -1,12 +1,17 @@
 package com.main.application.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MainApplication {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(MainApplication.class);
+
 	public static void main(String[] args) {
+		LOGGER.info("========== Run School Application =============");
 		SpringApplication.run(MainApplication.class, args);
 	}
 
