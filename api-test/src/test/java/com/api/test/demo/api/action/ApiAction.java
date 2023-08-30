ce5293429af8278d868a825393156c5b95d816fa
randy steven <randysteven12@gmail.com>
2023-08-30 13:14:43 +0700

    api test prepare

--- api-test/src/test/java/com/api/test/demo/api/action/ApiAction.java
+++ api-test/src/test/java/com/api/test/demo/api/action/ApiAction.java
@@ -1,14 +1,22 @@
 package com.api.test.demo.api.action;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.hc.client5.http.classic.methods.HttpGet;
 import org.apache.hc.client5.http.classic.methods.HttpPost;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.HttpResponse;
 import org.apiguardian.api.API;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
 
 public abstract class ApiAction {
 
@@ -19,6 +27,8 @@ public abstract class ApiAction {
     protected static CloseableHttpClient httpClient;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ApiAction.class);
+
+    ObjectMapper objectMapper = new ObjectMapper();
 
     public String apiServicePort(String port){
         return API_HOST + port;
@@ -46,5 +56,24 @@ public abstract class ApiAction {
         HttpGet get = new HttpGet(url);
         return get;
     }
+
+    protected String createRequestBody(String jsonPath, Map<String, Object> contextRewrite) throws
+            FileNotFoundException, IOException {
+        String requestBodyJson = new String(Files.readAllBytes(Paths.get(jsonPath)));
+        Map<String, Object> dataMap = objectMapper.readValue(requestBodyJson, Map.class);
+        for(Map.Entry<String, Object> context : contextRewrite.entrySet()) {
+            dataMap.put(context.getKey(), context.getValue());
+        }
+        String updatedRequestBody = objectMapper.writeValueAsString(dataMap);
+        return updatedRequestBody;
+    }
+
+    public abstract HttpResponse post(String endpoint, Map<String, String> requestHeader, String requestBody);
+
+    public abstract HttpResponse post(String endpoint, Map<String, String> requestHeader, JSONObject requestBody);
+
+    public abstract HttpResponse post(String endpoint, Map<String, String> requestHeader, Map<String, String> requestBody);
+
+    public abstract HttpResponse get(String endpoint, Map<String, String> requestHeader);
 
 }
--- api-test/src/test/java/com/api/test/demo/api/action/CommonApiAction.java
+++ api-test/src/test/java/com/api/test/demo/api/action/CommonApiAction.java
@@ -1,4 +1,33 @@
 package com.api.test.demo.api.action;
 
-public class CommonApiAction {
+import org.apache.hc.core5.http.HttpResponse;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+
+public class CommonApiAction extends ApiAction{
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CommonApiAction.class);
+
+    @Override
+    public HttpResponse post(String endpoint, Map<String, String> requestHeader, String requestBody) {
+        return null;
+    }
+
+    @Override
+    public HttpResponse post(String endpoint, Map<String, String> requestHeader, JSONObject requestBody) {
+        return null;
+    }
+
+    @Override
+    public HttpResponse post(String endpoint, Map<String, String> requestHeader, Map<String, String> requestBody) {
+        return null;
+    }
+
+    @Override
+    public HttpResponse get(String endpoint, Map<String, String> requestHeader) {
+        return null;
+    }
 }
