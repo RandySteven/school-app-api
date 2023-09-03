b216b233fa4486307f059c59445d3730052801fa
randy steven <randysteven12@gmail.com>
2023-09-03 16:56:31 +0700

    fixing

--- api-gateway/src/main/java/com/api/gateway/demo/configs/GatewayConfig.java
+++ api-gateway/src/main/java/com/api/gateway/demo/configs/GatewayConfig.java
@@ -1,39 +1,39 @@
-package com.api.gateway.demo.configs;
-
-import com.api.gateway.demo.enums.ServicePath;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.cloud.gateway.config.conditional.ConditionalOnEnabledPredicate;
-import org.springframework.cloud.gateway.handler.predicate.PathRoutePredicateFactory;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class GatewayConfig {
-
-    @Bean
-    public RouteLocatorBuilder routeLocatorBuilder(ConfigurableApplicationContext context){
-        return new RouteLocatorBuilder(context);
-    }
-
-    @Bean
-    @ConditionalOnEnabledPredicate
-    public PathRoutePredicateFactory pathRoutePredicateFactory() {
-        return new PathRoutePredicateFactory();
-    }
-
-    @Bean
-    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-        return builder.routes()
-                .route(ServicePath.STUDENT_SERVICE.getServiceName(), r -> r
-                        .path(ServicePath.STUDENT_SERVICE.getPrefix())
-                        .uri(ServicePath.STUDENT_SERVICE.getUri()))
-                .route(ServicePath.COURSE_SERVICE.getServiceName(), r -> r
-                        .path(ServicePath.COURSE_SERVICE.getPrefix())
-                        .uri(ServicePath.COURSE_SERVICE.getUri()))
-                .build();
-    }
-
-}
+//package com.api.gateway.demo.configs;
+//
+//import com.api.gateway.demo.enums.ServicePath;
+//import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+//import org.springframework.cloud.gateway.config.conditional.ConditionalOnEnabledPredicate;
+//import org.springframework.cloud.gateway.handler.predicate.PathRoutePredicateFactory;
+//import org.springframework.cloud.gateway.route.RouteLocator;
+//import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+//import org.springframework.context.ConfigurableApplicationContext;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class GatewayConfig {
+//
+//    @Bean
+//    public RouteLocatorBuilder routeLocatorBuilder(ConfigurableApplicationContext context){
+//        return new RouteLocatorBuilder(context);
+//    }
+//
+//    @Bean
+//    @ConditionalOnEnabledPredicate
+//    public PathRoutePredicateFactory pathRoutePredicateFactory() {
+//        return new PathRoutePredicateFactory();
+//    }
+//
+//    @Bean
+//    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+//        return builder.routes()
+//                .route(ServicePath.STUDENT_SERVICE.getServiceName(), r -> r
+//                        .path(ServicePath.STUDENT_SERVICE.getPrefix())
+//                        .uri(ServicePath.STUDENT_SERVICE.getUri()))
+//                .route(ServicePath.COURSE_SERVICE.getServiceName(), r -> r
+//                        .path(ServicePath.COURSE_SERVICE.getPrefix())
+//                        .uri(ServicePath.COURSE_SERVICE.getUri()))
+//                .build();
+//    }
+//
+//}
