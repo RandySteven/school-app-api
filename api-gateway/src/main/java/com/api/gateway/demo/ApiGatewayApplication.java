3224fa7f5860f0ee69c59c298f1e2f0341a877d9
randy steven <randysteven12@gmail.com>
2023-09-08 15:04:16 +0700

    spring api gateway dev complate

--- api-gateway/src/main/java/com/api/gateway/demo/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/api/gateway/demo/ApiGatewayApplication.java
@@ -6,11 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
-@EnableEurekaClient
 public class ApiGatewayApplication {
 
 //	@Autowired
--- student-service/src/main/java/com/student/service/demo/StudentServiceApplication.java
+++ student-service/src/main/java/com/student/service/demo/StudentServiceApplication.java
@@ -5,11 +5,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class StudentServiceApplication {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(StudentServiceApplication.class);
